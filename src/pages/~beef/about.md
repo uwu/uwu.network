356dc0180b77015c717de5e166f33442239af355
Beef <beefers@riseup.net>
2022-05-31 23:49:47 +0100

    [Users > Beef] Add work on my personal page

---
+++ src/pages/~beef/about.md
@@ -0,0 +1,12 @@
+---
+title: Who am I?
+layout: ../../layouts/beef/IndexLayout.astro
+---
+
+# Hi there!
+
+I'm Beef, a developer from Britain, who specialises in **TypeScript** and **JavaScript**. I love to build innovative new things, and am generally a friendly person.
+
+## TODO
+
+Write more here!
--- windi.config.ts
+++ windi.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "windicss/helpers";
+import * as catpuccin from "./catppuccin.json";
 
 export default defineConfig({
   preflight: true, // set false to disable removal of browser default styles
   theme: {
     extend: {
       fontFamily: { // class="font-plex"
-          plex: ["IBM Plex Mono", "monospace"]
+          plex: ["IBM Plex Mono", "monospace"],
+          inter: ["Inter", "sans-serif"]
+      },
+      colors: {
+          ...catpuccin
       }
     },
   },

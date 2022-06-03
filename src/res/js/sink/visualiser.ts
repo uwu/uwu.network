107a1c0593f9919fa29c1c24e8f950fc9e0cc09f
Cain Atkinson <yellowsink@riseup.net>
2022-06-03 11:34:10 +0100

    [Users > sink] more visualiser work TODO lead

--- src/res/js/sink/visualiser.ts
+++ src/res/js/sink/visualiser.ts
@@ -10,62 +10,69 @@ const hideState = (dur: number): Transition => ({
   ms: dur - 50,
 });
 
-const sleep = (t: number) => new Promise((r) => setTimeout(r, t));
+const for_ = (t: number) => new Promise((r) => setTimeout(r, t));
 
 const at = (x: number, y: number) => document.getElementById(`${x}-${y}`);
 
-const show = (x: number, y: number, dur = 250) => {
+function show(x: number, y: number, dur = 250) {
   at(x, y).doTransition(showState);
   at(x, y).doTransition(hideState(dur));
-};
+}
+
+function showArea(x1: number, x2: number, y1: number, y2: number, dur = 250) {
+  for (let y = y1; y <= y2; y++) for (let x = x1; x <= x2; x++) show(x, y, dur);
+}
+
+const BPM = 160;
+const beatDivision = (div: number) => 1000 * ((60 / BPM) / (div / 2));
 
 export async function Synths() {
   let lastT = 0;
-  const wait = (t: number) => {
-    const prom = sleep(t - lastT);
+  const until = (t: number) => {
+    const prom = for_(t - lastT);
     lastT = t;
     return prom;
   };
 
   async function main() {
     show(4, 4);
-    await sleep(1000);
+    await for_(1000);
     show(5, 5);
-    await sleep(1500 - 1000);
+    await for_(1500 - 1000);
     show(5, 4);
-    await sleep(2450 - 1500);
+    await for_(2450 - 1500);
     show(4, 5);
-    await sleep(3000 - 2440);
+    await for_(3000 - 2440);
     show(4, 4);
-    await sleep(3950 - 3000);
+    await for_(3950 - 3000);
     show(5, 5);
-    await sleep(4500 - 3950);
+    await for_(4500 - 3950);
     show(5, 4);
   }
 
   async function trill() {
-    show(5, 3);
-    await sleep(200);
     show(4, 3);
-    await sleep(200);
-    show(3, 4);
+    await for_(200);
+    show(5, 3);
+    await for_(200);
+    show(6, 4);
   }
 
   main();
-  await wait(5400);
+  await until(5400);
   trill();
-  await wait(6000);
+  await until(6000);
   lastT = 0;
 
   while (true) {
     main();
-    await wait(5400);
+    await until(5400);
     trill();
-    await wait(6000);
+    await until(6000);
     main();
-    await wait(11500);
+    await until(11500);
     show(4, 5);
-    await wait(12000);
+    await until(12000);
     lastT = 0;
   }
 }
@@ -74,24 +81,45 @@ export async function Bass() {
   async function run(x: number, y: number) {
     for (let i = 0; i < 4; i++) {
       show(x, y);
-      await sleep(375);
+      await for_(375);
     }
   }
 
-  await sleep(12200);
+  await for_(12200);
   while (true) {
-    await run(8, 7);
     await run(7, 7);
-    await run(8, 7);
-    await run(7, 7);
-    await run(8, 6);
-    await run(7, 7);
-    await run(8, 7);
     await run(6, 7);
+    await run(7, 7);
+    await run(6, 7);
+    await run(7, 6);
+    await run(6, 7);
+    await run(7, 7);
+    await run(5, 7);
+  }
+}
+
+export async function BassDrum() {
+  // actually starts at 6000 but animation moment
+  await for_(6000 - 25);
+
+  while (true) {
+    showArea(0, 2, 0, 0)
+    await for_(beatDivision(2));
+  }
+}
+
+export async function Hat() {
+  await for_(12000 - 25);
+
+  while (true) {
+    showArea(3, 5, 0, 0, 130);
+    await for_(beatDivision(4));
   }
 }
 
 export default () => {
   Synths();
   Bass();
-}
+  BassDrum();
+  Hat();
+};

59d83cf8b8c689790198faf4551dccc94a4cd43a
yellowsink <yellowsink@riseup.net>
2022-06-17 18:08:07 +0100

    [Meta] setup react for alyxia

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import WindiCSS from "vite-plugin-windicss";
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
-import solid from "@astrojs/solid-js";
+//import solid from "@astrojs/solid-js";
+import react from "@astrojs/react";
+//import preact from "@astrojs/preact"; // this would need installing via npm
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), svelte(), solid(), sitemap()],
+  integrations: [vue(), svelte(), /* solid(), */ /* preact(), */ react(), sitemap()],
   site: "https://uwu.network",
   vite: {
     plugins: WindiCSS()

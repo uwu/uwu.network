f1759eb38b86740e3e3cf76c65408c3c8dc3f6c3
yellowsink <yellowsink@riseup.net>
2022-10-25 15:47:39 +0100

    [Users > sink] astro dev and build being different moment

--- src/pages/~sink/quiet.rss.js
+++ src/pages/~sink/quiet.rss.js
@@ -1,15 +1,9 @@
 import getRSS from "@astrojs/rss";
 
-export const get = async () => {
-  const { body } = await getRSS({
+export const get = () =>
+  getRSS({
     title: "Quiet System",
     description: "Yellowsink's blog - code, unix, and ramblings.",
     site: new URL("~sink/blog/", import.meta.env.SITE).href,
     items: import.meta.glob("./blog/**/*.md"),
   });
-
-  const headers = new Headers();
-  headers.set("Content-Type", "application/rss+xml");
-
-  return new Response(body, { headers });
-};

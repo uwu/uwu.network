ee8fb004cfd2b78023be6b0f403e91b03d111db2
redstonekasi <kasi@hanneskann.de>
2022-11-01 20:55:00 +0100

    [Users > alyxia] Better parser

--- src/res/js/alyxia/parse.ts
+++ src/res/js/alyxia/parse.ts
@@ -2,8 +2,21 @@ import { TextParser } from "cc-textparser";
 
 interface ColorCommand {
   index: number;
+  type: "COLOR";
   color: string;
 }
+
+interface UnderlineCommand {
+  index: number;
+  type: "UNDERLINE";
+}
+
+interface MarkupInstruction {
+  index: number;
+  insert: string;
+}
+
+type Command = ColorCommand | UnderlineCommand;
 
 const colorMap = [
   "#ffffff", // white (default)
@@ -14,35 +27,125 @@ const colorMap = [
   "#ff8932", // orange (small font)
 ];
 
-const parser = new TextParser<[ColorCommand[]]>();
+const parser = new TextParser<[Command[]]>();
 
 parser.registerCommand("\\", false, () => "\\");
-parser.registerCommand("c", true, (color, index, colors) =>
-  void colors.push({ index, color }));
+parser.registerCommand("c", true, (color, index, cmds) =>
+  void cmds.push({ index, type: "COLOR", color }));
+parser.registerCommand("u", false, (index, cmds) =>
+  void cmds.push({ index, type: "UNDERLINE" }));
 
-export function parse(input: string) {
-  const colors: ColorCommand[] = [];
-  const parsed = parser.parse(input, colors);
-  
-  let result = "";
-  let pos = 0;
-  let inside = false;
-  for (const cmd of colors) {
-    result += parsed.substring(pos, cmd.index);
-    if (inside)
-      result += "</span>";
-    if (cmd.color !== "0") {
-      result += `<span style="color: ${colorMap[cmd.color]}">`
-      inside = true;
-    } else {
-      if (inside)
-        inside = false;
+export function analyzeColors(cmds: Command[]): MarkupInstruction[] {
+  const result = [];
+  let span = false;
+
+  for (const cmd of cmds) {
+    if (cmd.type !== "COLOR") continue;
+    const instruction: MarkupInstruction = {
+      index: cmd.index,
+      insert: "",
     }
-    pos = cmd.index;
+
+    if (span)
+      instruction.insert += "</span>"
+    if (cmd.color !== "0") {
+      instruction.insert += `<span style="color: ${colorMap[cmd.color] ?? cmd.color}">`;
+      span = true;
+    } else if (span)
+      span = false;
+    
+    result.push(instruction);
   }
-  result += parsed.substring(pos);
-  if (inside)
-    result += "</span>"
+
+  if (span)
+    result.push({ index: Infinity, insert: "</span>" });
 
   return result;
 }
+
+export function analyzeUnderlines(cmds: Command[]): MarkupInstruction[] {
+  const result = [];
+  let underlined = false;
+  
+  for (const cmd of cmds) {
+    if (cmd.type !== "UNDERLINE") continue;
+    const instruction: MarkupInstruction = {
+      index: cmd.index,
+      insert: underlined ? "</u>" : "<u>",
+    }
+
+    underlined = !underlined;
+    result.push(instruction);
+  }
+
+  if (underlined)
+    result.push({ index: Infinity, insert: "</u>" });
+
+  return result;
+}
+
+// takes 0.08ms - 0.25ms time
+export function parse(input: string) {
+  console.time("parse")
+  const cmds: Command[] = [];
+  const parsed = parser.parse(input, cmds);
+  
+  let instructions: MarkupInstruction[] = [];
+  instructions.push(...analyzeColors(cmds));
+  instructions.push(...analyzeUnderlines(cmds));
+  instructions = instructions.sort((a, b) => a.index - b.index);
+
+  let result = "";
+  let pos = 0;
+  for (const instruction of instructions) {
+    result += parsed.substring(pos, instruction.index);
+    result += instruction.insert
+    pos = instruction.index;
+  }
+
+  result += parsed.substring(pos);
+console.timeEnd("parse")
+  return result;
+}
+
+// takes 0.04ms - 0.15ms time
+// export function parse(input: string) {
+//   const cmds: Command[] = [];
+//   const parsed = parser.parse(input, cmds);
+
+//   let result = "";
+//   let pos = 0;
+
+//   let span = false;
+//   let underlined = false;
+
+//   for (const cmd of cmds) {
+//     result += parsed.substring(pos, cmd.index);
+
+//     switch (cmd.type) {
+//       case "COLOR":
+//         if (span)
+//           result += "</span>";
+//         if (cmd.color !== "0") {
+//           result += `<span style="color: ${colorMap[cmd.color]}">`
+//           span = true;
+//         } else {
+//           if (span)
+//             span = false;
+//         }
+//         break;
+//       case "UNDERLINE":
+//         result += underlined ? "</u>" : "<u>";
+//         underlined = !underlined;
+//         pos = cmd.index;
+//         break;
+//     }
+
+//     pos = cmd.index;
+//   }
+//   result += parsed.substring(pos);
+//   if (span)
+//     result += "</span>"
+
+//   return result;
+// }

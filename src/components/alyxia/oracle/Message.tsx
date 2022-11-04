bcf0f7f2a3311e1acd96dc716a73b3cad4ed078f
Alyxia Sother <alyxia@riseup.net>
2022-11-05 00:58:55 +0100

    [Users > alyxia] Fix passing content into message

--- src/components/alyxia/oracle/Message.tsx
+++ src/components/alyxia/oracle/Message.tsx
@@ -3,13 +3,14 @@ import React from "react";
 interface MessageProps {
     name: string;
     side: string;
+    text: string;
 }
-export const Message = ({ name, side, children }: React.PropsWithChildren<MessageProps>) => {
+export const Message = ({ name, side, text }: MessageProps) => {
     return (
         <div className={`mesom m${side}`}>
             <img src={`/alyxia/img/oracle/${name}.png`} alt="" />
             <div>
-                <b>{name.toUpperCase()}</b> {children}
+                <b>{name.toUpperCase()}</b> <p>{text}</p>
             </div>
         </div>
     )

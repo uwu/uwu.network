428ec3b668be68818fc92157d57c9a03e566fb1a
toonlink <toonlink@riseup.net>
2022-11-05 10:38:01 -0400

    complete toonlink rename

--- src/components/toonlink/TerminalPrompt.vue
+++ src/components/toonlink/TerminalPrompt.vue
@@ -9,7 +9,7 @@ defineProps({
 <template>
   <div class="flex justify-between">
     <div>
-      creatable@<span class="underline">home</span>>{{ value
+      toonlink@<span class="underline">home</span>>{{ value
       }}<span v-if="!hideCaret" class="term-caret font-bold">_</span>
     </div>
     <div class="text-green-400">~</div>

3e59b79d0af85f1eea9e62091f02f76f52d2a209
redstonekasi <kasi@hanneskann.de>
2023-11-18 16:40:49 +0100

    [janitor] clean up dependencies

---
+++ astro.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import vue from "@astrojs/vue";
+import mdx from "@astrojs/mdx";
+import uno from "@unocss/astro";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue(), sitemap(), mdx(), uno()],
+  site: "https://uwu.network",
+});

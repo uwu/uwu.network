bbc9fca79d18dbbadc6f6cd7e84fadeee1469f7c
Hazel Atkinson <yellowsink@riseup.net>
2026-01-02 18:36:08 +0000

    [meta] fix build

---
+++ src/res/js/fleximg-impl.ts
@@ -0,0 +1,60 @@
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
+import imageSize from "image-size";
+import { imageSizeFromFile } from "image-size/fromFile";
+import { dirname, resolve } from "path";
+import { env } from "process";
+import { fileURLToPath } from "url";
+
+export type Size = Omit<
+	ReturnType<typeof import("image-size").imageSize>,
+	"images"
+>;
+
+const thisModulePath = fileURLToPath(import.meta.url);
+
+export let rootPath = thisModulePath;
+while (!existsSync(resolve(rootPath, "package.json")) && rootPath.split("/").length > 1) {
+	rootPath = dirname(rootPath);
+}
+
+const cachePath = resolve(rootPath, "node_modules/.cache/uwu/img-sizes");
+
+const cacheData: Record<string, Size> = existsSync(cachePath)
+	? JSON.parse(readFileSync(cachePath).toString())
+	: {};
+
+function addToCache(src: string, size: Size) {
+	cacheData[src] = size;
+	mkdirSync(dirname(cachePath), { recursive: true });
+	writeFileSync(cachePath, JSON.stringify(cacheData));
+}
+
+async function getSizeImpl(src: string) {
+	// first, look up in cache
+	let size = cacheData[src];
+
+	if (!size) {
+		let isUrl = !!src.match(/^(http(s?))?:?\/\//);
+
+		if (isUrl && import.meta.env.DEV) {
+			console.log(`[FlexImg] caching image ${src}...`);
+		}
+
+		size = isUrl
+			? imageSize(new Uint8Array(await fetch(src).then(r => r.arrayBuffer()) as ArrayBuffer))
+			: await imageSizeFromFile(resolve(rootPath, "public/", src.slice(1)));
+
+		addToCache(src, size);
+	}
+
+	return size;
+}
+
+// only run one promise at a time to avoid timeouts
+let promise: Promise<unknown> = Promise.resolve();
+
+export async function getSize(src: string) {
+	await promise;
+	return (promise = getSizeImpl(src));
+}
+

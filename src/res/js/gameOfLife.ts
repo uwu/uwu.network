01a735cbeaed8f3d68c534b9b0403104ce717b57
xirreal <me@xirreal.dev>
2026-01-07 14:52:23 +0100

    uwu.network, im the one who shades

--- src/res/js/gameOfLife.ts
+++ src/res/js/gameOfLife.ts
@@ -1,186 +1,193 @@
 /*\
 |*| Conway's Game of Life implemented in TypeScript
 |*| using just <div>s
-|*| written by Cain Atkinson // Yellowsink 2022
+|*| written by Hazel Atkinson // Yellowsink 2022
 |*| for uwu.network
 |*|
 |*| Pls credit if you use thanks!!!
+|*| hi xirreal here i made some small changes but this is basically the same as before, please credit Hazel not me
 \*/
 
 // CONSTANTS
 const CONTAINER_ID = "gol",
-  ROW_CLASS = "gol-row",
-  CELL_CLASS = "gol-cell",
-  ACTIVE_CLASS = "gol-on",
-  SIMPLE_CLASS = "gol-simple",
-  SIMPLE_B_ID = "gol-fade",
-  RAND_B_ID = "gol-rand",
-  PAUSE_B_ID = "gol-stop",
-  CLEAR_B_ID = "gol-clear",
-  RAND_THRES = 0.8;
-
-const container = document.getElementById(CONTAINER_ID)!;
+	ROW_CLASS = "gol-row",
+	CELL_CLASS = "gol-cell",
+	ACTIVE_CLASS = "gol-on",
+	SIMPLE_CLASS = "gol-simple",
+	SIMPLE_B_ID = "gol-fade",
+	RAND_B_ID = "gol-rand",
+	PAUSE_B_ID = "gol-stop",
+	CLEAR_B_ID = "gol-clear",
+	RAND_THRES = 0.8;
 
 class Grid2D {
-  store: boolean[][] = [];
-  width: number;
-  height: number;
+	store: boolean[][] = [];
+	width: number;
+	height: number;
+	container?: HTMLElement;
 
-  constructor(width: number, height: number) {
-    this.width = width;
-    this.height = height;
+	constructor(width: number, height: number, container?: HTMLElement) {
+		this.width = width;
+		this.height = height;
+		this.container = container;
 
-    this.resize(width, height);
-  }
+		this.resize(width, height);
+	}
 
-  wrapCoords(x: number, y: number) {
-    if (x === this.width) x = 0;
-    if (y === this.height) y = 0;
-    if (x < 0) x = this.width - 1;
-    if (y < 0) y = this.height - 1;
+	wrapCoords(x: number, y: number) {
+		if (x === this.width) x = 0;
+		if (y === this.height) y = 0;
+		if (x < 0) x = this.width - 1;
+		if (y < 0) y = this.height - 1;
 
-    return [x, y];
-  }
+		return [x, y];
+	}
 
-  at(x: number, y: number) {
-    [x, y] = this.wrapCoords(x, y);
-    return this.store[y][x];
-  }
+	at(x: number, y: number) {
+		[x, y] = this.wrapCoords(x, y);
+		return this.store[y][x];
+	}
 
-  set(x: number, y: number, val: boolean) {
-    [x, y] = this.wrapCoords(x, y);
-    this.store[y][x] = val;
-  }
+	set(x: number, y: number, val: boolean) {
+		[x, y] = this.wrapCoords(x, y);
+		this.store[y][x] = val;
+	}
 
-  resize(width: number, height: number) {
-    const newGrid: boolean[][] = [];
+	resize(width: number, height: number) {
+		const newGrid: boolean[][] = [];
 
-    for (let y = 0; y < height; y++) {
-      const row = new Array(width);
-      newGrid[y] = row;
+		for (let y = 0; y < height; y++) {
+			const row = new Array(width);
+			newGrid[y] = row;
 
-      for (let x = 0; x < width; x++) {
-        row[x] = this.store[y]?.[x] ?? false;
-      }
-    }
+			for (let x = 0; x < width; x++) {
+				row[x] = this.store[y]?.[x] ?? false;
+			}
+		}
 
-    this.store = newGrid;
-    this.width = width;
-    this.height = height;
-  }
+		this.store = newGrid;
+		this.width = width;
+		this.height = height;
+	}
 
-  randomize() {
-    for (let y = 0; y < this.store.length; y++)
-      for (let x = 0; x < this.store[y].length; x++)
-        this.store[y][x] = Math.random() > RAND_THRES;
-  }
+	randomize() {
+		for (let y = 0; y < this.store.length; y++)
+			for (let x = 0; x < this.store[y].length; x++) this.store[y][x] = Math.random() > RAND_THRES;
+	}
 
-  copyFrom(grid: Grid2D) {
-    // lol
-    this.store = grid.store;
-  }
-}
+	copyFrom(grid: Grid2D) {
+		// lol
+		this.store = grid.store;
+	}
 
-const grid = new Grid2D(0, 0);
+	/** creates all cell divs in the container */
+	createCells() {
+		if (!this.container) return;
+		this.container.innerHTML = "";
 
-/** creates all cell divs in the container */
-function createCells() {
-  container.innerHTML = "";
+		for (let y = 0; y < this.height; y++) {
+			const rowElem = document.createElement("div");
+			this.container.appendChild(rowElem);
+			rowElem.className = ROW_CLASS;
 
-  for (let y = 0; y < grid.height; y++) {
-    const rowElem = document.createElement("div");
-    container.appendChild(rowElem);
-    rowElem.className = ROW_CLASS;
+			for (let x = 0; x < this.width; x++) {
+				const cell = document.createElement("div");
+				rowElem.appendChild(cell);
+				cell.className = CELL_CLASS;
 
-    for (let x = 0; x < grid.width; x++) {
-      const cell = document.createElement("div");
-      rowElem.appendChild(cell);
-      cell.className = CELL_CLASS;
+				cell.onclick = () => {
+					this.set(x, y, !this.at(x, y));
+					this.render();
+				};
+			}
+		}
+	}
 
-      cell.onclick = () => {
-        grid.set(x, y, !grid.at(x, y));
-        renderCells();
-      };
-    }
-  }
-}
+	/** Writes the game state to the divs */
+	render() {
+		if (!this.container) return;
 
-/** Writes the game state to the divs */
-function renderCells() {
-  for (let y = 0; y < container.childElementCount; y++) {
-    const row = container.children[y];
+		for (let y = 0; y < this.container.childElementCount; y++) {
+			const row = this.container.children[y];
 
-    for (let x = 0; x < row.childElementCount; x++)
-      row.children[x].classList.toggle(ACTIVE_CLASS, grid.at(x, y));
-  }
-}
+			for (let x = 0; x < row.childElementCount; x++) row.children[x].classList.toggle(ACTIVE_CLASS, this.at(x, y));
+		}
+	}
 
-function cellSurvives(x: number, y: number) {
-  let liveNeighbors = 0;
+	survives(x: number, y: number) {
+		let liveNeighbors = 0;
 
-  if (grid.at(x - 1, y - 1)) liveNeighbors++;
-  if (grid.at(x, y - 1)) liveNeighbors++;
-  if (grid.at(x + 1, y - 1)) liveNeighbors++;
-  if (grid.at(x - 1, y)) liveNeighbors++;
-  if (grid.at(x + 1, y)) liveNeighbors++;
-  if (grid.at(x - 1, y + 1)) liveNeighbors++;
-  if (grid.at(x, y + 1)) liveNeighbors++;
-  if (grid.at(x + 1, y + 1)) liveNeighbors++;
+		if (this.at(x - 1, y - 1)) liveNeighbors++;
+		if (this.at(x, y - 1)) liveNeighbors++;
+		if (this.at(x + 1, y - 1)) liveNeighbors++;
+		if (this.at(x - 1, y)) liveNeighbors++;
+		if (this.at(x + 1, y)) liveNeighbors++;
+		if (this.at(x - 1, y + 1)) liveNeighbors++;
+		if (this.at(x, y + 1)) liveNeighbors++;
+		if (this.at(x + 1, y + 1)) liveNeighbors++;
 
-  return liveNeighbors === 3 || (liveNeighbors === 2 && grid.at(x, y));
-}
+		return liveNeighbors === 3 || (liveNeighbors === 2 && this.at(x, y));
+	}
 
-function tick() {
-  const newGrid = new Grid2D(grid.width, grid.height);
+	tick() {
+		const newGrid = new Grid2D(this.width, this.height);
 
-  for (let y = 0; y < grid.height; y++)
-    for (let x = 0; x < grid.width; x++) {
-      newGrid.set(x, y, cellSurvives(x, y));
-    }
+		for (let y = 0; y < this.height; y++)
+			for (let x = 0; x < this.width; x++) {
+				newGrid.set(x, y, this.survives(x, y));
+			}
 
-  grid.copyFrom(newGrid);
+		this.copyFrom(newGrid);
+	}
 }
 
 // these heights are only used to generate cell counts nothing more
 export default (cellWidth: number, cellHeight: number) => {
-  function makeGrid() {
-    const gridWidth = Math.floor(window.innerWidth / cellWidth);
-    const gridHeight = Math.floor(window.innerHeight / cellHeight);
+	const container = document.createElement("div");
+	container.id = CONTAINER_ID;
+	const canvas = document.getElementById(CONTAINER_ID)!;
 
-    grid.resize(gridWidth, gridHeight);
-    createCells();
-    renderCells();
-  }
+	canvas.parentNode?.replaceChild(container, canvas);
 
-  window.addEventListener("resize", makeGrid);
-  makeGrid();
-  grid.randomize();
-  renderCells();
+	const grid = new Grid2D(0, 0, container);
 
-  let tickingPaused = false;
+	function makeGrid() {
+		const gridWidth = Math.floor(window.innerWidth / cellWidth);
+		const gridHeight = Math.floor(window.innerHeight / cellHeight);
 
-  setInterval(() => {
-    if (tickingPaused) return;
-    tick();
-    renderCells();
-  }, 100);
+		grid.resize(gridWidth, gridHeight);
+		grid.createCells();
+		grid.render();
+	}
 
-  document.getElementById(SIMPLE_B_ID)!.onclick = () =>
-    container.classList.toggle(SIMPLE_CLASS);
+	window.addEventListener("resize", makeGrid);
+	makeGrid();
+	grid.randomize();
+	grid.render();
 
-  document.getElementById(RAND_B_ID)!.onclick = () => {
-    grid.randomize();
-    renderCells();
-  };
+	let tickingPaused = false;
 
-  const pauseButton = document.getElementById(PAUSE_B_ID)!;
-  pauseButton.onclick = () => {
-    tickingPaused = !tickingPaused;
-    pauseButton.innerText = pauseButton.innerText === "play" ? "pause" : "play";
-  };
+	setInterval(() => {
+		if (tickingPaused) return;
+		grid.tick();
+		grid.render();
+	}, 100);
 
-  document.getElementById(CLEAR_B_ID)!.onclick = () => {
-    grid.resize(0, 0);
-    makeGrid();
-  };
+	document.getElementById(SIMPLE_B_ID)!.onclick = () => container.classList.toggle(SIMPLE_CLASS);
+
+	document.getElementById(RAND_B_ID)!.onclick = () => {
+		grid.randomize();
+		grid.render();
+	};
+
+	const pauseButton = document.getElementById(PAUSE_B_ID)!;
+	pauseButton.onclick = () => {
+		tickingPaused = !tickingPaused;
+		pauseButton.innerText = pauseButton.innerText === "play" ? "pause" : "play";
+	};
+
+	document.getElementById(CLEAR_B_ID)!.onclick = () => {
+		grid.resize(0, 0);
+		makeGrid();
+	};
 };
